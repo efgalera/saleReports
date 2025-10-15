66acabc218025f8dbc35585518c1ede2d8509a92
Emilio Galera <emiliofgalera@gmail.com>
2025-10-15 00:23:21 -0300

    Basic functionality

--- src/processor/_DataProcessor.py
+++ src/processor/_DataProcessor.py
@@ -1,4 +1,4 @@
-from typing import List, Union
+from typing import Dict
 
 from interfaces import DataReaderInterface
 
@@ -8,3 +8,38 @@ class DataProcessor:
         data_reader: DataReaderInterface
     ) -> None:
         self.data_reader = data_reader
+
+    def get_total_sale_by_product(
+        self,
+        product: str,
+    ) -> int:
+
+        res = 0
+        # TODO: the processor should not  know names or index of the dataset
+        for row in self.data_reader.get_rows(
+            column="produto",
+            value=product,
+            ):
+            res += row[1]
+
+        return res
+
+    def get_net_value(self) -> int:
+
+        res = 0
+        for row in self.data_reader.read():
+            res += row[2]
+
+        return res
+
+    def get_most_frequent_product(self) -> Dict[str, int]:
+
+        grouping = {}
+        for row in self.data_reader.read():
+            if row[0] not in grouping:
+                grouping.update({row[0]: row[1]})
+            else:
+                grouping[row[0]] += row[1]
+
+        sorted_prods = sorted([(x, y) for x, y in grouping.items()], key=lambda z: z[1])
+        return sorted_prods[-1][0]
---
+++ tests/test_data_processor.py
@@ -0,0 +1,29 @@
+import pytest
+
+from src.processor import DataProcessor
+
+from .utils import MockDataReader
+
+
+@pytest.fixture
+def processor():
+    mock_reader = MockDataReader(
+        path_to_data="path/to/somewhere"
+    )
+    return DataProcessor(
+        data_reader=mock_reader
+    )
+
+def test_sale_by_product(processor: DataProcessor):
+    total_sale_a = processor.get_total_sale_by_product(product="a")
+    total_sale_b = processor.get_total_sale_by_product(product="b")
+    total_sale_c = processor.get_total_sale_by_product(product="c")
+    assert [total_sale_a, total_sale_b, total_sale_c] == [3, 2, 5]
+
+def test_sale_value(processor: DataProcessor):
+    net_value = processor.get_net_value()
+    assert net_value == 21
+
+def test_most_frequent_product(processor: DataProcessor):
+    prod = processor.get_most_frequent_product()
+    assert prod == "c"
